24d6d22ced1e08d5c2bce188fa8b8fcf57cc4870
JinyuBiao <143368950+JinyuBiao@users.noreply.github.com>
2023-12-06 10:43:38 -0500

    delete unused attributes

--- COMP3004-with-picture/dataProcessor.h
+++ COMP3004-with-picture/dataProcessor.h
@@ -76,16 +76,6 @@ class DataProcessor
         bool ChildPad;//default to false
         bool AdultPad;//default to false
 
-        /*
-        bool signalECG;				//defaults true
-        bool functionalElectrode;	//default to true
-        bool functionalMicroptocessor;		//default to true
-        bool functionalSoftware;			//default to true
-        bool functionalCompressionDepthDetector;	//default to true
-        bool functionalVoicePrompt;			//default to true
-        bool functionalVisualIndicator;		//default to true
-        */
-
         StateType detectedState;
 };
 
